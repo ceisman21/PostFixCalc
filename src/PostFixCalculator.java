fc7e243341f8d006bf211b9d0744d8723a75b148
ceisman21 <ceisman21@kentdenver.org>
2020-11-02 12:51:51 -0700

    Basics Added

--- src/PostFixCalculator.java
+++ src/PostFixCalculator.java
@@ -1,2 +1,23 @@
 public class PostFixCalculator {
+    String input;
+    String[] inputArray
+    public PostFixCalculator(String input) {
+        this.input = input;
+    }
+    private void stringTokenizer() {
+        
+    }
+    private int adder(int a, int b) {
+        return 0;
+    }
+    private int subtractor(int a, int b) {
+        return 0;
+    }
+    private int multiplier(int a, int b) {
+        return 0;
+    }
+    private int divider(int a, int b) {
+        return 0;
+    }
+
 }
--- src/PostFixTester.java
+++ src/PostFixTester.java
@@ -1,2 +1,6 @@
 public class PostFixTester {
+    public static void main(String[] args) {
+        PostFixCalculator PFC = new PostFixCalculator("1 1 +");
+        System.out.print("This is where we'll test the PostFixCalc");
+    }
 }
