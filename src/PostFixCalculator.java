108a0cd3ccb66f6ce699a3388efeb2866bbb91df
ceisman21 <ceisman21@kentdenver.org>
2020-11-02 13:12:02 -0700

    getResult Done

--- src/PostFixCalculator.java
+++ src/PostFixCalculator.java
@@ -1,4 +1,9 @@
-import java.util.StringTokenizer;
+/*
+Cooper Eisman and Addison Hoff
+Date Created: 11/2/2020
+Date Modified: 11/2/2020
+Creates a Stack of StackNode type object
+*/
 
 public class PostFixCalculator {
     String input;
@@ -8,7 +13,33 @@ public class PostFixCalculator {
         inputArray = input.split("\\s");
     }
     public int getResult() {
-
+        Stack numStack = new Stack();
+        for(int x = 0; inputArray.length > x; x++) {
+            if (inputArray[x].equals("+")) {
+                int a = (int)(numStack.pop());
+                int b = (int)(numStack.pop());
+                numStack.push(adder(a,b));
+            }
+            else if (inputArray[x].equals("-")) {
+                int a = (int)(numStack.pop());
+                int b = (int)(numStack.pop());
+                numStack.push(subtractor(a,b));
+            }
+            else if (inputArray[x].equals("*")) {
+                int a = (int)(numStack.pop());
+                int b = (int)(numStack.pop());
+                numStack.push(multiplier(a,b));
+            }
+            else if (inputArray[x].equals("/")) {
+                int a = (int)(numStack.pop());
+                int b = (int)(numStack.pop());
+                numStack.push(divider(a,b));
+            }
+            else {
+                numStack.push(inputArray[x]);
+            }
+        }
+        return (int)(numStack.pop());
     }
     private int adder(int a, int b) {
         return 0;
--- src/PostFixTester.java
+++ src/PostFixTester.java
@@ -1,3 +1,10 @@
+/*
+Cooper Eisman and Addison Hoff
+Date Created: 11/2/2020
+Date Modified: 11/2/2020
+Creates a Stack of StackNode type object
+*/
+
 public class PostFixTester {
     public static void main(String[] args) {
         PostFixCalculator PFC = new PostFixCalculator("1 1 +");
